e000aba298cecc9d876d9e26c80d1c639140c8aa
Emmanuel Ogbizi-Ugbe <iamogbz+github@gmail.com>
2020-01-09 22:34:04 -0500

    wip: invoke commands

--- Makefile
+++ Makefile
@@ -1,5 +1,4 @@
 $(shell test -s ".env" || cp ".env.example" ".env")
-ENVARS := $(shell cat ".env" | xargs)
 
 .PHONY: upstream
 upstream:
@@ -33,26 +32,9 @@ venv:
 
 .PHONY: install
 install: venv
-	venv/bin/pip install --upgrade pip
-	venv/bin/pip install -Ur requirements.txt
-
-.PHONY: tests
-tests:
-	env ${ENVARS} pytest
-
-.PHONY: test
-test:
-	env ${ENVARS} pytest -s -k $(keyword)
-
-.PHONY: coverage
-coverage:
-	@env ${ENVARS} coverage run --source=. -m pytest
-	@coverage html
-
-.PHONY: build
-build:
-	@echo "Nothing to do"
-	@mkdir ./artifacts && echo "Draft build" > ./artifacts/build
+	venv/bin/python -m pip install -U pip
+	venv/bin/python -m pip install -Ur requirements.txt
+	venv/bin/python -m python_githooks
 
 ifndef VERBOSE
 .SILENT:
---
+++ tasks.py
@@ -0,0 +1,66 @@
+import os
+
+from invoke import exceptions, task
+
+
+@task
+def clean(ctx):
+    """
+    Remove build files e.g. package, distributable, compiled etc.
+    """
+    ctx.run(f"rm -rf *.egg-info dist build __pycache__ .pytest_cache artifacts/*")
+
+
+@task
+def lint(ctx, fix=False):
+    """
+    Check and fix syntax
+    """
+    lint_commands = {
+        "isort": f"python -m isort {'' if fix else '--check-only --diff'} -y",
+        "black": f"python -m black {'' if fix else '--check'} .",
+        "flake8": "python -m flake8 src tests *.py",
+        "mypy": "python -m mypy --strict src",
+    }
+    last_error = None
+    for section, command in lint_commands.items():
+        print(f"\033[1m[{section}]\033[0m")
+        try:
+            ctx.run(command, pty=True)
+        except exceptions.Failure as ex:
+            last_error = ex
+        print()
+    if last_error:
+        raise last_error
+
+
+@task(
+    help={
+        "coverage": "Build and report on test coverage",
+        "dev": "Use syrupy development version",
+        "test-pattern": "Pattern used to select test files to run",
+        "verbose": "Verbose output e.g. non captured logs etc.",
+    }
+)
+def test(ctx, coverage=False, dev=False, test_pattern=None, verbose=False):
+    """
+    Run entire test suite
+    """
+    env = {"PYTHONPATH": "./src"} if dev else {}
+    flags = {"-s -vv": verbose, f"-k {test_pattern}": test_pattern}
+    coverage_module = "coverage run -m " if coverage else ""
+    test_flags = " ".join(flag for flag, enabled in flags.items() if enabled)
+    ctx.run(f"python -m {coverage_module}pytest {test_flags} .", env=env, pty=True)
+    if coverage:
+        if not os.environ.get("CI"):
+            ctx.run("coverage report", pty=True)
+        else:
+            ctx.run("codecov", pty=True)
+
+
+@task(pre=[clean])
+def build(ctx):
+    """
+    Generate version from scm and build package distributable
+    """
+    # ctx.run("python setup.py sdist bdist_wheel")
